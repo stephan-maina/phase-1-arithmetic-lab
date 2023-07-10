71ed001a6bbdb53fc29b8ca6c078b366aea3d289
stephan-maina <135037455+stephan-maina@users.noreply.github.com>
2023-07-10 03:39:02 +0300

    Add files via upload

---
+++ phase-1-arithmetic-lab.js
@@ -0,0 +1,25 @@
+var a = 10;
+var b = 20;
+var c = a + b;
+console.log(c); // 30
+
+var a = 10;
+var b = 20;
+var c = a - b;
+console.log(c); // -10
+
+var a = 10;
+var b = 20;
+var c = a * b;
+console.log(c); // 200
+
+var a = 15;
+var b = 3;
+var c = a / b;
+console.log(c); // 5
+
+var a = 10;
+var b = 2;
+var c = a % b;
+console.log(c); // 0
+
